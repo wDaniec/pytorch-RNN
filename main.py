b33571a6cb05f2e5cbad178f9d0a56025bda5e48
wDaniec <andrzejZdobywca@gmail.com>
2020-07-20 20:07:11 +0200

    Working network - Need some optimization

--- main.py
+++ main.py
@@ -4,24 +4,25 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.utils.data import Dataset, DataLoader
+import time
 
-device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 EMB_SIZE = 12
 HIDDEN_SIZE = 96
-BATCH_SIZE = 4
+BATCH_SIZE = 16
 
 class MyDataset(Dataset):
 
     def __init__(self, csv_file):
         self.csv = np.array(pd.read_csv(csv_file, sep=',', header=None))
-
+        self.csv = torch.Tensor([[[get_start_embeds(int(x)) for x in my_input] for my_input in problem] for problem in self.csv]).long()
     def __len__(self):
         return self.csv.shape[0]
 
     def __getitem__(self, idx):
-        res = self.csv[idx]
-        res = torch.Tensor([[int(x) for x in sudoku] for sudoku in res]).long()
-        return res
+        # res = torch.Tensor([[int(x) for x in sudoku] for sudoku in self.csv[idx]]).long()
+        # return res
+        return self.csv[idx]
 
 def get_edges():
     def cross(a):
@@ -37,13 +38,16 @@ def get_edges():
             squares += cross(idx[i * 3:(i + 1) * 3, j * 3:(j + 1) * 3])
     return torch.Tensor(list(set(rows + columns + squares))).long()
 
-def get_start_embeds(embed, embed_dict, start_state):
+def get_start_embeds(embed, start_state):
     start_state = torch.Tensor([x.item() for x in start_state]).long().to(device)
-    X = embed(start_state)
+    rows = torch.Tensor([i // 9 for i in range(81)]).long().to(device)
+    columns = torch.Tensor([i % 9 for i in range(81)]).long().to(device)
+    X = torch.cat([embed(start_state, EMB_SIZE), embed(rows, EMB_SIZE), embed(columns, EMB_SIZE)], dim=1).float().to(device)
+     
     return X
     
-def get_start_embeds_batched(embed, embed_dict, start_state_batched):
-    return torch.stack([get_start_embeds(embed, embed_dict, start_state) for start_state in start_state_batched])
+def get_start_embeds_batched(embed, start_state_batched):
+    return torch.stack([get_start_embeds(embed, start_state) for start_state in start_state_batched])
 
 def message_passing(nodes, edges, message_fn):
     n_nodes = nodes.shape[0]
@@ -63,11 +67,11 @@ def message_passing_batched(nodes_batched, edges, message_fn):
     return torch.stack([message_passing(nodes, edges, message_fn) for nodes in nodes_batched])
 
 class MLP(nn.Module):
-    def __init__(self):
+    def __init__(self, input_size):
         super(MLP, self).__init__()
-        self.fc_in = nn.Linear(2*EMB_SIZE, HIDDEN_SIZE)
+        self.fc_in = nn.Linear(input_size, HIDDEN_SIZE)
         self.fc = nn.Linear(HIDDEN_SIZE, HIDDEN_SIZE)
-        self.fc_out = nn.Linear(HIDDEN_SIZE, EMB_SIZE)
+        self.fc_out = nn.Linear(HIDDEN_SIZE, HIDDEN_SIZE)
     def forward(self, x):
         x = F.relu(self.fc_in(x))
         x = F.relu(self.fc(x))
@@ -75,18 +79,10 @@ class MLP(nn.Module):
         x = self.fc_out(x)
         return x
 
-class MyLSTM(nn.Module):
-    def __init__(self):
-        super(MyLSTM, self).__init__()
-        self.fc1 = nn.Linear(3*EMB_SIZE, EMB_SIZE)
-    def forward(self, x):
-        x = self.fc1(x)
-        return x
-
 class Pred(nn.Module):
-    def __init__(self):
+    def __init__(self, input_size):
         super(Pred, self).__init__()
-        self.fc1 = nn.Linear(EMB_SIZE, 10)
+        self.fc1 = nn.Linear(input_size, 10)
     def forward(self, x):
         x = self.fc1(x)
         return x
@@ -94,51 +90,61 @@ class Pred(nn.Module):
 def one_hot(num):
     return torch.Tensor
 
-traindataset = MyDataset('sample.csv')
-trainloader = DataLoader(traindataset, batch_size = 4, shuffle = True, num_workers = 4)
+traindataset = MyDataset('train.csv')
+trainloader = DataLoader(traindataset, batch_size = BATCH_SIZE, shuffle = True, num_workers = 4)
 
-embed = nn.Embedding(10, EMB_SIZE).to(device)
-word_to_emb = {i: i for i in range(10)}
-message_fn = MLP().to(device)
-g = nn.LSTM(3*EMB_SIZE, EMB_SIZE, HIDDEN_SIZE, batch_first=True).to(device)
-r = Pred().to(device)
-optimizer_embed = torch.optim.Adam(embed.parameters(), lr=2e-4, weight_decay=1e-4)
+mlp1 = MLP(3*EMB_SIZE).to(device)
+mlp2 = MLP(2*HIDDEN_SIZE).to(device)
+mlp3 = MLP(2*HIDDEN_SIZE).to(device)
+r = Pred(HIDDEN_SIZE).to(device)
+lstm = nn.LSTM(HIDDEN_SIZE, HIDDEN_SIZE, batch_first=True).to(device)
+embed = torch.nn.functional.one_hot
+
+
+optimizer_mlp1 = torch.optim.Adam(mlp1.parameters(), lr=2e-4, weight_decay=1e-4)
+optimizer_mlp2 = torch.optim.Adam(mlp2.parameters(), lr=2e-4, weight_decay=1e-4)
+optimizer_mlp3 = torch.optim.Adam(mlp3.parameters(), lr=2e-4, weight_decay=1e-4)
 optimizer_r = torch.optim.Adam(r.parameters(), lr=2e-4, weight_decay=1e-4)
-optimizer_g = torch.optim.Adam(g.parameters(), lr=2e-4, weight_decay=1e-4)
-optimizer_mlp = torch.optim.Adam(message_fn.parameters(), lr=2e-4, weight_decay=1e-4)
-optimizers = [optimizer_embed, optimizer_r, optimizer_g, optimizer_mlp]
+optimizer_lstm = torch.optim.Adam(lstm.parameters(), lr=2e-4, weight_decay=1e-4)
 
+optimizers = [optimizer_mlp1, optimizer_mlp2, optimizer_mlp3, optimizer_r, optimizer_lstm]
 criterion = nn.CrossEntropyLoss()
 
-for epoch in range(50):
-    for start_state_batched in trainloader:
+edges = get_edges()
+
+start_time = time.time()
+for epoch in range(1000):
+    for batch_id, start_state_batched in enumerate(trainloader):
         Y = start_state_batched[:, 1, :].to(device)
         X = start_state_batched[:, 0, :].to(device)
 
-        X = get_start_embeds_batched(embed, word_to_emb, X)
+        X = get_start_embeds_batched(embed, X)
+        X = mlp1(X)
         H = X.detach().clone().to(device)
 
-        edges = get_edges()
 
         for optimizer in optimizers:
             optimizer.zero_grad()
         
         loss = 0
-        S = torch.zeros(HIDDEN_SIZE, BATCH_SIZE, EMB_SIZE).to(device)
-        S = (S, S)
+        S = torch.zeros(1, X.shape[0], HIDDEN_SIZE).to(device)
         for i in range(32):
-            M = message_passing_batched(H, edges, message_fn)
-            res = torch.cat([H, X, M], dim=2)
-            H, S = g(res, S)
-            # print(S[0].shape, S[1].shape)
-            # print(H.shape)
+            H = message_passing_batched(H, edges, mlp2) # message_fn
+            H = mlp3(torch.cat([H, X], dim=2))
+            H, S = lstm(H)
             res = r(H)
-            # pred = nn.Softmax(dim=2)(res)
+            
             pred = res
             for j in range(Y.shape[0]):
                 # print(loss)
                 loss += criterion(pred[j], Y[j])
-            loss /= Y.shape[0]
+                # print("step ", i, ": ",loss)
+            # print(loss)
+        loss /= Y.shape[0]
+        if(batch_id % 10 == 0):
+            print("10 batches time:", time.time() - start_time)
+            start_time = time.time()
+        print(batch_id, '/', len(trainloader))
         print(loss)
         loss.backward()
         for optimizer in optimizers:
